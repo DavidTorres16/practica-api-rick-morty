83ab4ae9e809c7ad92a4ef4c8837be1147d1e7ac
David Torres <dmtg16@hotmail.com>
2021-09-16 16:50:15 -0500

    Primer commit DT Se creó las distribución de carpetas, el modulo basicHome y la base del componente cards

---
+++ src/modules/BasicHome/Card.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+export default function Card() {
+    return (
+        <div>
+            <div className="col1">
+                <img src="" alt=""></img>
+            </div>
+            <div className="col2">
+                <div className="tituloNombre">
+                    <h2></h2>
+                    <p></p>
+                </div>
+                <div className="ultimaLocacion">
+                    <h3></h3>
+                    <p></p>
+                </div>
+                <div className="primeraVezVisto">
+                    <h3></h3>
+                    <p></p>
+                </div>
+            </div>
+        </div>
+    )
+}
